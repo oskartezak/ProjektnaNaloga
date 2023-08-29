a380f816db4dbedc170d982120619fc732539ff6
klemzium <133033343+klemzium@users.noreply.github.com>
2023-08-29 15:23:48 +0200

    Add files via upload

---
+++ poskus.py
@@ -0,0 +1,15 @@
+#W
+planeti_in_lune = 'https://en.wikipedia.org/wiki/List_of_Star_Wars_planets_and_moons'
+#html izbrane strani
+planeti_in_lune_html = 'Planeti_in_lune.html'
+
+#csv datoteka obdelanih podatkov
+csv_characters = 'osebe.csv'
+csv_knjige = 'knjige.csv'
+csv_dolžine_knjig = 'dolžine.csv'
+
+#mapa, v katero spravimo neobdelane (html) podatke
+directory_neobdelani = 'podatki-neobdelani'
+
+#mapa, v katero sppravimo obdelane (csv) podatke
+directory_obdelani = 'podatki-obdelani'
